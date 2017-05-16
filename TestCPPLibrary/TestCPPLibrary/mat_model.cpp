3c33d996eca7460a098b77c897e13682d326d3b2
DementyevMaxim <m_dementyev@mail.ru>
2017-05-16 23:42:13 +0300

    add_initialization_koef

--- TestCPPLibrary/TestCPPLibrary/mat_model.cpp
+++ TestCPPLibrary/TestCPPLibrary/mat_model.cpp
@@ -1,5 +1,19 @@
 #include "stdafx.h"
 
-extern "C" __declspec(dllexport) Vector2  initialization {
+extern "C" __declspec(dllexport) koef_of_model  initialization() {
+	koef_of_model koef_model;
 
+	FILE *in;
+	if ((in = fopen("model_koef.txt", "r")) == NULL) {
+		printf("Cannot open file model_koef.txt");
+		return koef_model;
+	}
+
+	fscanf(in, "%lf", &koef_model.mass);
+	fscanf(in, "%lf", &koef_model.gravity);
+	fscanf(in, "%lf", &koef_model.force);
+	
+	fclose(in);
+	return koef_model;
 }
+
