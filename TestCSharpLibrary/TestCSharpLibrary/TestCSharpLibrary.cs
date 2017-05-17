d4c2bf9def1ec426233e94325ea04744e7a82236
DementyevMaxim <m_dementyev@mail.ru>
2017-05-18 00:12:19 +0300

    Deleted superfluous

--- TestCSharpLibrary/TestCSharpLibrary/TestCSharpLibrary.cs
+++ TestCSharpLibrary/TestCSharpLibrary/TestCSharpLibrary.cs
@@ -18,25 +18,5 @@ namespace TestCSharpLibrary
 
         [DllImport("TestCPPLibrary")]
         public static extern float TestSum(float a, float b);
-
-
-        public static float SharpMultiply(float a, float b)
-        {
-            return (a * b);
-        }
-
-        public static float SharpDivide(float a, float b)
-        {
-            if (b == 0)
-            {
-                return 0;
-            }
-
-            return (a / b);
-        }
-        public static float SharpSum(float a, float b)
-        {
-            return (a + b);
-        }
     }
 }
