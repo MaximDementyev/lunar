e079f10f0ab7f514893ee190fa898d0c31f7b133
DementyevMaxim <m_dementyev@mail.ru>
2017-06-21 08:16:56 +0300

    version 1.0

--- TestCPPLibrary/TestCPPLibrary/TestCPPLibrary.cpp
+++ TestCPPLibrary/TestCPPLibrary/TestCPPLibrary.cpp
@@ -22,3 +22,24 @@ extern "C" __declspec(dllexport) float TestSum(float a, float b)
 {
 	return a + b;
 }
+
+extern "C" __declspec(dllexport) struct Vector2 TestStruct()
+{
+	Vector2 res;
+	res.x = 5;
+	res.y = 7;
+	FILE *out = fopen("cpp_out.txt", "w");
+	fprintf(out, "Vector2.x = %lf\nVector2.y = %lf", res.x, res.y);
+	fclose(out);
+	return res;
+}
+
+extern "C" __declspec(dllexport) void TestUpdateStruct(struct Vector2* res)
+{
+	FILE *out = fopen("cpp_out.txt", "a");
+	fprintf(out, "\n________\nUpdate in\nVector2.x = %lf\nVector2.y = %lf", res->x, res->y);
+	res->x = 1;
+	res->y = 10;
+	fprintf(out, "\n________\nUpdate out\nVector2.x = %lf\nVector2.y = %lf", res->x, res->y);
+	fclose(out);
+}
