80d35221b9e7e00113e310d4886d90fbbbf8ee81
Dev-B-V-A <bobylev.v.a@yandex.ru>
2017-06-23 01:39:33 +0300

    Revert "REF: refactoring dll functions, code style, some rewrite logic"
    This reverts commit 011178b2d74e57be5c192937a49a29697154bd23.

--- TestCPPLibrary/TestCPPLibrary/function_header.h
+++ TestCPPLibrary/TestCPPLibrary/function_header.h
@@ -2,39 +2,28 @@
 #include "stdafx.h"
 
 //bibl.cpp
-// maybe remove same functions
-double norm (const struct Vector2 &, const struct Vector2 &);
-double norm (const struct Vector2 *, const struct Vector2 *);
-
-double norm (const struct Vector2 &);
-double norm (const struct Vector2 *);
-
-double sign (const double);
-double cos_deg (const double);
-double sin_deg (const double);
-double tan_deg (const double);
-double dotprod (const struct Vector2 &, const struct Vector2 &);
+double norm(struct Vector2, struct Vector2);
+double norm(struct Vector2*, struct Vector2*);
+double norm(struct Vector2);
+double norm(struct Vector2*);
+double sign(double);
+double cos_deg(double);
+double sin_deg(double);
+double tan_deg(double);
+double dotprod(struct Vector2, struct Vector2);
 
 //runge.cpp
-void next_step_N (struct state_model *, struct koef_of_model *, struct surface *, double *, double *);
-void next_step_no_N (struct state_model*, struct koef_of_model*, double *);
+void next_step_N(struct state_model*, struct koef_of_model*, struct surface*, double* force, double* time_left);
+void next_step_no_N(struct state_model*, struct koef_of_model*, double *time);
 
 //runge_func.cpp
-struct Vector2 func_solve_acceleration (const struct Vector2 *, const struct koef_of_model *, double *, struct surface *);
-void runge_koef (const struct Vector2 *, const struct Vector2 *, const struct koef_of_model*, struct surface*,
-				 double *, double *, struct runge_K *K); // Attention * &
-struct Vector2 solve_koef_coord (const struct runge_K *);
-struct Vector2 solve_koef_velocity (const struct runge_K *);
+struct Vector2 func_solve_acceleration(struct Vector2* Velocity, struct koef_of_model*, double* force, struct surface*);
+void runge_koef(struct Vector2* solve_velocity, struct Vector2* solve_acceleration, struct koef_of_model*, struct surface*, double* force, double* step, struct runge_K* K); // Attention * &
+struct Vector2 solve_koef_coord(struct runge_K*);
+struct Vector2 solve_koef_velocity(struct runge_K*);
 
 //xz.cpp
-enum class touch_earth
-{
-	normal = 0, // Through how many will be touching
-	outside_surface, // Touching outside a known surface
-	fall_down_texture, // We fell through the texture
-};
-
-touch_earth find_earth (const struct state_model *, const struct koef_of_model *, const struct surface *, double *); 
-double time_no_N (const struct state_model *, const struct surface *);
-void hit (struct state_model*, struct koef_of_model*, const struct surface *);
-int touch_test (const struct state_model *, const struct surface *);
+double find_earth(struct state_model*, struct koef_of_model*, struct surface*); //-2 error! We fell through the texture //-1 Touching outside a known surface //Through how many will be touching
+double time_no_N(struct state_model*, struct surface*);
+void hit(struct state_model*, struct koef_of_model*, struct surface*);
+int touch_test(struct state_model*, struct surface*);
--- TestCPPLibrary/TestCPPLibrary/xz.cpp
+++ TestCPPLibrary/TestCPPLibrary/xz.cpp
@@ -1,31 +1,26 @@
 #include"stdafx.h"
 const double eps = 1e-5; //The error of touching the wheel of the earth
 
-touch_earth find_earth (const state_model *current_model, const koef_of_model *koef_model, const surface *current_surface, double *res) {
+double find_earth(state_model* current_model, koef_of_model* koef_model, surface* current_surface) {
 	double b = tan_deg(current_surface->angle * current_model->Velocity.x - current_model->Velocity.y); //Coefficient before the first power in the quadratic equation
 	double D = b * b / 2 * koef_model->gravity * (tan_deg(current_surface->angle) * current_model->Coord.x + koef_model->radius - current_model->Coord.y); //Discriminant
-	if (D < 0)
-		return touch_earth::fall_down_texture; //error! We fell through the texture
+	if (D < 0) return -2; //error! We fell through the texture
 	
 	double touch_time = (-b + sqrt(D)) / koef_model->gravity; //Surface touch time
 	double find_tau = (current_model->Coord.x + current_model->Velocity.x * touch_time) / current_surface->limitation_x; //Touch point
-	if (find_tau < 0 || find_tau > 1)
-		return touch_earth::outside_surface; // Touching outside a known surface
+	if (find_tau < 0 || find_tau > 1) return -1; // Touching outside a known surface
 
-	*res = touch_time;
-	return touch_earth::normal;
+	return touch_time;
 }
 
-double time_no_N (const state_model *current_model, const surface *current_surface)
-{
+double time_no_N(state_model* current_model, surface* current_surface) {
 	if (current_model->Velocity.x > 0)
 		return (current_surface->start_x + current_surface->limitation_x - current_model->Coord.x) / current_model->Velocity.x;
 	else
 		return (current_surface->start_x - current_model->Coord.x) / current_model->Velocity.x;
 }
 
-// TODO: maybe koef_model unused variable?
-void hit (state_model* current_model, koef_of_model* koef_model, surface* current_surface) {
+void hit(state_model* current_model, koef_of_model* koef_model, surface* current_surface) {
 	struct Vector2 guide_surface; //Surface vector guide
 	guide_surface.x = cos_deg(current_surface->angle);
 	guide_surface.y = sin_deg(current_surface->angle);
@@ -33,8 +28,7 @@ void hit (state_model* current_model, koef_of_model* koef_model, surface* curren
 	current_model->Velocity = dotprod(current_model->Velocity, guide_surface) / norm(guide_surface) * guide_surface; //find progection
 }
 
-int touch_test (const state_model *current_model, const surface *current_surface)
-{
+int touch_test(state_model* current_model, surface* current_surface) {
 	double ñurrent_surface_height = tan_deg(current_surface->angle) * current_model->Coord.x + current_surface->srart_y - current_surface->start_x * tan_deg(current_surface->angle);
 	if (ñurrent_surface_height > current_model->Coord.y) return -1;
 	if (fabs(ñurrent_surface_height - current_model->Coord.y) < eps) return 1;
