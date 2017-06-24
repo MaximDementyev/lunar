5e4d833c0e2d390ec6e2f408126969374199d6e7
DementyevMaxim <m_dementyev@mail.ru>
2017-06-24 15:58:27 +0300

    correct reference

--- Koala/Assets/Scripts/Scripts/PlayerMovement.cs
+++ Koala/Assets/Scripts/Scripts/PlayerMovement.cs
@@ -7,7 +7,6 @@ using System;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public StreamWriter log; 
     public Camera playerCamera;
 	public struct double_Vector2{
 		public double x;
@@ -36,14 +35,14 @@ public class PlayerMovement : MonoBehaviour
 	public koef_of_model koef_model;
 
 	[DllImport("TestCPPLibrary",CallingConvention = CallingConvention.Cdecl, EntryPoint="solve_step")]
-	public static unsafe extern int solve_step(state_model* current_model, koef_of_model* koef_model, surface* current_surface, double* step_time, double* force);
+	public static unsafe extern int solve_step(ref state_model current_model, ref koef_of_model koef_model, ref surface current_surface, ref double step_time, ref double force);
 	public state_model current_model;
 
 	public Vector2 current_position;
 	public double force_module = 10;
 
 	public Vector2 bias_ray, vec_gravity;
-
+	public int tmp_test;
 
     void Start()
     {
@@ -73,16 +72,16 @@ public class PlayerMovement : MonoBehaviour
 		current_surface.limitation_x = 0.1;
 		current_surface.mu = 1;
 
-		//log = new StreamWriter (@"log_unity.txt");
 		//StreamWriter log = new StreamWriter(@"log_unity_start.txt");
 		//log.WriteLine("current_position.x = " + current_position.x + "\ncurrent_position.y = " + current_position.y);
 		//log.WriteLine("\n\ncurrent_model.Coord.x = " + current_model.Coord.x + "\ncurrent_model.Coord.y = " + current_model.Coord.y);
 		//log.Close();
+		tmp_test = 0;
     }
 
 
 	void Update(){
-		this.transform.position = current_position;
+		GetComponent<Transform>().position = new Vector3(current_position.x, current_position.y, 0);
 
 		//After we move, adjust the camera to follow the player
 		playerCamera.transform.position = new Vector3(transform.position.x, transform.position.y + 10, playerCamera.transform.position.z);
@@ -90,7 +89,8 @@ public class PlayerMovement : MonoBehaviour
 	}
 
     void FixedUpdate()
-    {    
+	{   
+		tmp_test++;
 		if (Input.GetKey(KeyCode.A))
 		{
 			++force_module;
@@ -100,75 +100,66 @@ public class PlayerMovement : MonoBehaviour
 		{
 			if (force_module > 0) --force_module;
 		}
-		double force;
+		double force = 0;
 		if (Input.GetKeyDown (KeyCode.RightArrow) || Input.GetKeyDown (KeyCode.LeftArrow) ) {
 			if (Input.GetKeyDown (KeyCode.RightArrow)) force = force_module;
 			if (Input.GetKeyDown (KeyCode.LeftArrow)) force = -force_module;
-		} else {
-			force = 0;
 		}
 
-
-		double step_time = (double)Time.deltaTime;
+		double step_time = 0.1;//(double)Time.deltaTime;
 		int res_solve;
-		/*state_model current_model;
-		current_model.Velocity.x = 0;
-		current_model.Velocity.y = 0;
-		current_position = this.transform.position;
-		current_model.Coord.x = System.Convert.ToDouble(current_position.x);
-		current_model.Coord.y = System.Convert.ToDouble(current_position.y);*/
 		unsafe{
-			
-			//state_model* ptr_current_model = &current_model;
-			fixed(state_model* ptr_current_model = &current_model) {
-				fixed(koef_of_model *ptr_koef_model = &koef_model) {
-					fixed(surface *ptr_current_surface = &current_surface) {
-						//log.WriteLine("\n2current_model.Coord.x = " + ptr_current_model->Coord.x + "\n1current_model.Coord.y = " + ptr_current_model->Coord.y);
-						//log.WriteLine("\n\n!current_model.Coord.x = " + current_model.Coord.x + "\ncurrent_model.Coord.y = " + current_model.Coord.y);
-
-						if (current_model.Velocity.x >= 0) learn_the_surface(ptr_current_surface, ptr_current_model, 1);
-						else learn_the_surface(ptr_current_surface, ptr_current_model, -1);
-						/*while ((res_solve = solve_step(ptr_current_model, ptr_koef_model, ptr_current_surface, &step_time, &force)) != 0){
-							current_position.x = (float)current_model.Coord.x;
-							current_position.y = (float)current_model.Coord.y;
-							learn_the_surface(ptr_current_surface, ptr_current_model, res_solve);
-						}*/
-					}
-				}
+			StreamWriter log = new StreamWriter (@"log_unity.txt");
+			if (current_model.Velocity.x >= 0) learn_the_surface(ref current_surface, ref current_model, 1);
+			else learn_the_surface(ref current_surface, ref current_model, -1);
+			log.WriteLine("\n\ntmp_test = " + tmp_test + "\ncurrent_model.Coord.x = " + current_model.Coord.x + "\ncurrent_model.Coord.y = " + current_model.Coord.y);
+			while ((res_solve = solve_step(ref current_model, ref koef_model, ref current_surface, ref step_time, ref force)) != 0){
+				log.WriteLine("\n\ntmp_test = " + tmp_test + "\ncurrent_model.Coord.x = " + current_model.Coord.x + "\ncurrent_model.Coord.y = " + current_model.Coord.y);
+				current_position.x = (float)current_model.Coord.x;
+				current_position.y = (float)current_model.Coord.y;
+				learn_the_surface(ref current_surface, ref current_model, res_solve);
 			}
+			log.WriteLine("THE END\n");
+			log.Close();
 		}
 	}
 
-	unsafe void learn_the_surface (surface* current_surface, state_model* current_model, int flag){
-		double left_height = 100;
-		double right_height = 100;
+	unsafe void learn_the_surface (ref surface current_surface, ref state_model current_model, int flag){
+		double left_height = 0;
+		double right_height = 0;
 		if (flag == 1){
 			//reycast right
-			Raycasting(flag, &left_height, &right_height);
-			current_surface->start_x = current_model->Coord.x;
-			current_surface->start_y = current_model->Coord.y - left_height;
+			Raycasting(flag, ref left_height, ref right_height);
+			current_surface.start_x = current_model.Coord.x;
+			current_surface.start_y = current_model.Coord.y - left_height;
 		}else{
 			//reycast left
-			Raycasting(flag, &left_height, &right_height);
-			current_surface->start_x = current_model->Coord.x - current_surface->limitation_x;
-			current_surface->start_y =current_model->Coord.y - right_height;
+			Raycasting(flag, ref left_height, ref right_height);
+			current_surface.start_x = current_model.Coord.x - current_surface.limitation_x;
+			current_surface.start_y = current_model.Coord.y - right_height;
 		}
-		current_surface->angle = Math.Atan ((left_height - right_height) / current_surface->limitation_x);
+		StreamWriter log_ray2 = new StreamWriter (@"log_ray2.txt");
+		log_ray2.WriteLine("right = " + right_height + "\n");
+		log_ray2.Close();
+		current_surface.angle = Math.Atan ((left_height - right_height) / current_surface.limitation_x);
 	}
 
-	public unsafe void Raycasting(int flag, double* left_height, double* right_height){
+	public unsafe void Raycasting(int flag, ref double  left_height, ref double right_height){
 		RaycastHit hitSurfaceLeft;
 		RaycastHit hitSurfaceRight;
 		if (flag == 1) {
 			Physics.Raycast (current_position, vec_gravity, out hitSurfaceLeft);
 			Physics.Raycast (current_position + bias_ray, vec_gravity, out hitSurfaceRight);
-			*left_height = hitSurfaceLeft.distance;
-			*right_height = hitSurfaceRight.distance;
+			left_height = hitSurfaceLeft.distance;
+			right_height = hitSurfaceRight.distance;
 		} else {
 			Physics.Raycast (current_position, vec_gravity, out hitSurfaceRight);
 			Physics.Raycast (current_position - bias_ray, vec_gravity, out hitSurfaceLeft);
-			*left_height = hitSurfaceLeft.distance;
-			*right_height = hitSurfaceRight.distance;
+			left_height = hitSurfaceLeft.distance;
+			right_height = hitSurfaceRight.distance;
 		}
+		StreamWriter log_ray = new StreamWriter (@"log_ray.txt");
+		log_ray.WriteLine("right = " + right_height + "\n");
+		log_ray.Close();
 	}
 }
