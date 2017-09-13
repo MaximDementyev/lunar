7421534e68f23c6f701e12043898caa9cf649d62
DementyevMaxim <m_dementyev@mail.ru>
2017-09-13 22:34:03 +0300

    optimization of calculation acc_N (similar terms are given)

--- fiz2d/fiz2d/runge_func.cpp
+++ fiz2d/fiz2d/runge_func.cpp
@@ -5,31 +5,29 @@ const double eps = 1e-2; // подобрать
 
 Vector2 func_solve_acc_wheel(state_model* current_model, const Vector2* Velocity, const koef_of_model* koef_model, const double force, const surface* current_surface, const bool flag_record) {
 	double deformation_suspension = koef_model->wheel.position - norm(current_model->body.Coord, current_model->wheel.Coord);//We assume the deformation of the spring
+	double mg_Fyp = deformation_suspension * koef_model->wheel.rigidity_suspension + koef_model->wheel.mass * koef_model->world.gravity;
 	double acceleration;
 	struct Vector2 related_acc;
-	if (deformation_suspension * koef_model->wheel.rigidity_suspension + koef_model->wheel.mass * koef_model->world.gravity >= 0) {//The spring presses against the ground
+	if (mg_Fyp >= 0) {//The spring presses against the ground
 		if (norm(*Velocity) > eps) { //check zero speed
 			// We are moving. Resistance is directed against speed
 			//find acceleration in connected coordinate system
-			acceleration = (force -
-				current_surface->mu * (koef_model->wheel.mass * koef_model->world.gravity + deformation_suspension * koef_model->wheel.rigidity_suspension) * cos(current_surface->angle) * sign(Velocity->x) -
-				(koef_model->wheel.mass * koef_model->world.gravity + deformation_suspension * koef_model->wheel.rigidity_suspension) * sin(current_surface-> angle)
-				) / koef_model->wheel.mass;
+			acceleration = (force - mg_Fyp * (current_surface->mu * cos(current_surface->angle) * sign(Velocity->x) - sin(current_surface-> angle))) / koef_model->wheel.mass;
 			related_acc = rotate_vec(Vector2(acceleration, 0), current_surface->angle);
 		} else {
 			//tmp_force Just a convenient change of coordinates (All forces except friction on the connected x-axis = F-mg*sin(alpha))
-			double tmp_force = force - (koef_model->wheel.mass * koef_model->world.gravity + deformation_suspension * koef_model->wheel.rigidity_suspension) * sin(current_surface->angle);
-			if (fabs(tmp_force) <= fabs(current_surface->mu * (koef_model->wheel.mass * koef_model->world.gravity + deformation_suspension * koef_model->wheel.rigidity_suspension) * cos(current_surface->angle)))
+			double tmp_force = force - mg_Fyp * sin(current_surface->angle);
+			if (fabs(tmp_force) <= fabs(current_surface->mu * mg_Fyp * cos(current_surface->angle)))
 				related_acc.set0();// body is at rest
 			else {
 				//body rested, but began to move (Q>Ftr)
-				acceleration = (tmp_force - current_surface->mu * (koef_model->wheel.mass * koef_model->world.gravity + deformation_suspension * koef_model-> wheel.rigidity_suspension) * cos(current_surface->angle) * sign(tmp_force)) / koef_model->wheel.mass;
+				acceleration = (tmp_force - current_surface->mu * mg_Fyp * cos(current_surface->angle) * sign(tmp_force)) / koef_model->wheel.mass;
 				related_acc = rotate_vec(Vector2(acceleration, 0), current_surface->angle);
 			}
 		}
 	} else {
-		related_acc.x = (force - koef_model->wheel.mass * koef_model->world.gravity * sin(current_surface->angle)) / koef_model->wheel.mass;
-		related_acc.y = (-(koef_model->wheel.mass * koef_model->world.gravity + deformation_suspension * koef_model-> wheel.rigidity_suspension) * cos(koef_model->wheel.mass)) / koef_model->wheel.mass;
+		related_acc.x = (force - mg_Fyp * sin(current_surface->angle)) / koef_model->wheel.mass;
+		related_acc.y = (-mg_Fyp * cos(koef_model->wheel.mass)) / koef_model->wheel.mass;
 		related_acc = rotate_vec(related_acc, current_surface->angle);
 	}
 	if (flag_record == true) current_model->wheel.Acceleration = related_acc;
